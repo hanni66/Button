7e7c90ed4b7bb253dfdd4dbd3d209e48f887cc3a
hanni66 <haeunkim0807@gmail.com>
2023-03-17 13:04:12 +0900

    test react pj

---
+++ button/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import { Button } from 'hanni-button';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Button />
+    </div>
+  );
+}
+
+export default App;
